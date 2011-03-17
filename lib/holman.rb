1b8b1d0400e8db9360ee59a5f413646e032bb90c
Zach Holman <github.com@zachholman.com>
2011-03-17 00:32:06 -0700

    holman

---
+++ lib/holman.rb
@@ -0,0 +1,53 @@
+puts "
+ G G L L f f f j j j t t L t i i i i i i t t t t t j f L L L K # # W W
+ G L L f f f j j j t t t j j t j j i i i ; i i t t j j f L L W # # W K
+ L L f f f j j t t t t t D # # # t t j t ; ; i i i t j f f f K # # # E
+ G L f f j j t t j W # # # # # # # # K L t ; ; ; i t j f f f K # # # K
+ E L f f j j j f # # # # # # # # # # # # E i ; i i t j f f f W W # # E
+ L L f j j j D # # # # # # # # # # # # # # t ; ; i t t j f D W # W # K
+ f D j j j K # # # # # # # # # # # # W W E W , ; ; i t j f K W # W # K
+ f f j j K # # # # # # K E W K E K D D D E D L ; i t f G G W W # # K K
+ f f L L # # # # K E f j G f W D D , . . i E W j G D K E D # W # W t D
+ j j L # # # # K K L i , i : t t t         . # K L G D W E # W W W i L
+ j t f # # # # W D i : .                     E G f f L D # # W W W ; t
+ j t L # # # # W i : : .                     i E t j j j # K W W L ; ;
+ j t K # # W # L : : :                       : D i t i W # E W W ; ; ;
+ t t D # # # W L , : .                   i L ; G L G f # # W K W ; , ,
+ t t D # # W K G , : : i f W E j .   . f G : : K j t j # # K # t i ; ;
+ i i L # # # # G ; i L E L G L i :   . i ; i . # i : j # W W # ; ; ; ;
+ i ; G # # # # t i i t D W # j j .   , t ; W t D :   ; # W # W i ; ; i
+ i ; K # # # # i i j K t i t ; ; .     : .     ; :   : # W # G ; ; ; i
+ i ; f E # # # i i i ; : ; ;     .         .   :     : # K K i i i i t
+ i ; ; j L # K i : .           . .             . W # E W K K t i i i t
+ t t i t L # W i , .           . .               K W W K E K t i i t t
+ i ; ; i f i j i : .         .   .               D G # W K G t t t t j
+ t i ; i ; i i t , .     .   : j E D t t   : . . G W W       t t t t j
+ j t i ; ; , i t ; , : ; , . . , ; j i : .     , G # E f G j j j j j L
+ j t i ; , t t t i ; ; :   .       : .     .   f L # K L G f j j j j j
+ j t ; ; , , , t t : ; . : L j i ; ; t f ,     f G # # f G L j j j L t
+ j i ; ; ; , , t t ; . . .   : .       .     . f L # E L G L f j j j j
+ j i i ; ; , , i t i , , . . . . . . .       ; t E # E f G L f f L f f
+ t t i i ; ; , , j i i , ; . . .   . .     : W D # # W f G L f L t j j
+ j t i ; t # # # L j i ; ; , . . . . . .   , i # # # # E K D G W # # G
+ # # # # # # # # # D j i i ; , . . . . . . : . # # # # # # # # # # # #
+ # # # # # # # # # # E G j j i ; : . : : ; : . K # # # # # # # # # # #
+ # # # # # # # # # # G E E f j t i ; ; i t : : L # # # # # # # # # # #
+ # # # # # # # # # # K f L E E D G G f , , , : E # # # # # # # # # # #
+ # # # # # # # # # # # L f f L L f t ; i i , , # # # # # # # # # # # #
+"
+
+puts "
+       __                  __                                   
+      |  \\                |  \\                                  
+      | $$____    ______  | $$ ______ ____    ______   _______  
+      | $$    \\  /      \\ | $$|      \\    \\  |      \\ |       \\ 
+      | $$$$$$$\\|  $$$$$$\\| $$| $$$$$$\\$$$$\\  \\$$$$$$\\| $$$$$$$\\
+      | $$  | $$| $$  | $$| $$| $$ | $$ | $$ /      $$| $$  | $$
+      | $$  | $$| $$__/ $$| $$| $$ | $$ | $$|  $$$$$$$| $$  | $$
+      | $$  | $$ \\$$    $$| $$| $$ | $$ | $$ \\$$    $$| $$  | $$
+       \\$$   \\$$  \\$$$$$$  \\$$ \\$$  \\$$  \\$$  \\$$$$$$$ \\$$   \\$$
+"
+
+puts ""
+puts ""
+puts "      holman - http://twitter.com/holman - http://zachholman.com "
